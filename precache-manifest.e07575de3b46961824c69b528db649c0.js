7d075edc981794dc6c758a8c8ac9447cf783517b
Marissa <marissa.wong@outlook.com>
2020-05-06 16:50:46 -0300

    Updates

---
+++ precache-manifest.e07575de3b46961824c69b528db649c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af07290b82308501507354331c80a034",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffa6a9215c7016c4a398",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "87151403220af8700434",
+    "url": "/static/js/2.c867efc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c867efc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffa6a9215c7016c4a398",
+    "url": "/static/js/main.37296007.chunk.js"
+  },
+  {
+    "revision": "f6864fb8e0fb57f8c1fe",
+    "url": "/static/js/runtime-main.385da8d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
